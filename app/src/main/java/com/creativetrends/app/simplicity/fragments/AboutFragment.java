a3f99cc1e3d1ecdea07190bb1a0eb85d87659661
Jorell Rutledge <creativetrendzappsllc@gmail.com>
2017-12-24 15:47:32 -0500

    Update AboutFragment.java

--- app/src/main/java/com/creativetrends/app/simplicity/fragments/AboutFragment.java
+++ app/src/main/java/com/creativetrends/app/simplicity/fragments/AboutFragment.java
@@ -3,14 +3,11 @@ package com.creativetrends.app.simplicity.fragments;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
-import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ListView;
@@ -20,10 +17,6 @@ import com.creativetrends.app.simplicity.SimplicityApplication;
 import com.creativetrends.app.simplicity.utils.StaticUtils;
 import com.creativetrends.simplicity.app.R;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
 /**
  * Created by Creative Trends Apps.
  */
@@ -32,8 +25,6 @@ public class AboutFragment extends PreferenceFragment implements Preference.OnPr
     public boolean mListStyled;
     Context context;
     SharedPreferences preferences;
-    private static Elements eName;
-    private static Document dName;
     TextView etUsername;
 
     @Override
@@ -79,10 +70,6 @@ public class AboutFragment extends PreferenceFragment implements Preference.OnPr
         super.onPause();
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-    }
 
     @Override
     public boolean onPreferenceClick(Preference preference) {
@@ -98,72 +85,27 @@ public class AboutFragment extends PreferenceFragment implements Preference.OnPr
 
 
         }
-
         return false;
 
 
     }
 
-
-
-    @SuppressLint({"StaticFieldLeak", "SetTextI18n"})
-    private void updateChangelog() {
-        // Set the user name
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... params) {
-                try {
-                    dName = Jsoup.connect("https://play.google.com/store/apps/details?id=com.creativetrends.simplicity.app").get();
-                    eName = dName.select("div.recent-change").next();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(String string) {
-                try {
-                    if (eName != null) {
-                        etUsername.setText(Html.fromHtml(eName.toString().replace("&lt;b&gt;", " ")));
-                        Log.i("Changelog from Play", eName.text());
-                    }else{
-                        etUsername.setText("Bug fixes");
-                    }
-                } catch (NullPointerException ignored) {
-                } catch (Exception i) {
-                    i.printStackTrace();
-                }
-
-            }
-
-        }.execute();
-
-    }
-
     private void changeDialog() {
-        updateChangelog();
         LayoutInflater inflater = getActivity().getLayoutInflater();
         @SuppressLint("InflateParams") View alertLayout = inflater.inflate(R.layout.changelog_layout, null);
         etUsername = alertLayout.findViewById(R.id.change_text);
-        //etUsername.setText(Html.fromHtml(preferences.getString("new_change", "")));
+        etUsername.setText("Bug fixes");
         AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
         alert.setTitle(getString(R.string.whats_new));
         alert.setView(alertLayout);
-        // disallow cancel of AlertDialog on click of back button and outside touch
         alert.setCancelable(false);
         alert.setPositiveButton(getString(R.string.ok), (dialog, which) -> {
-
         });
-        try {
-            AlertDialog dialog = alert.create();
-            dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
-            dialog.show();
-        }catch(NullPointerException ignored){
-        }catch (Exception i){
-            i.printStackTrace();
+        alert.create();
+        if (!getActivity().isFinishing()) {
+            alert.show();
         }
-    }
 
+    }
 
 }
