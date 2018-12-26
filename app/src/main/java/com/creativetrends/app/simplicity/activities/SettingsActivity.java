0dd49c994d619036bc616cd8459de6103e8dfaad
Creative Trends <creativetrendzappsllc@gmail.com>
2018-12-25 19:27:08 -0500

    Update SettingsActivity.java

--- app/src/main/java/com/creativetrends/app/simplicity/activities/SettingsActivity.java
+++ app/src/main/java/com/creativetrends/app/simplicity/activities/SettingsActivity.java
@@ -8,9 +8,9 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
