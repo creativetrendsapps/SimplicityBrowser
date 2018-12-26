b0d04ad662c33b10ba4cd5d6d995efc9d3421efa
Creative Trends <creativetrendzappsllc@gmail.com>
2018-12-25 19:26:36 -0500

    Update HistoryActivity.java

--- app/src/main/java/com/creativetrends/app/simplicity/activities/HistoryActivity.java
+++ app/src/main/java/com/creativetrends/app/simplicity/activities/HistoryActivity.java
@@ -1,17 +1,15 @@
 package com.creativetrends.app.simplicity.activities;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
