b52fab25d08ac0ddcecb71b8b7072325227d96f3
Creative Trends <creativetrendzappsllc@gmail.com>
2019-08-06 12:44:27 -0400

    Update SimplicityApplication.java

--- app/src/main/java/com/creativetrends/app/simplicity/SimplicityApplication.java
+++ app/src/main/java/com/creativetrends/app/simplicity/SimplicityApplication.java
@@ -10,14 +10,11 @@ import android.preference.PreferenceManager;
 
 import com.creativetrends.simplicity.app.R;
 
-//import org.acra.ACRA;
 
 /**
  * Created by Creative Trends Apps.
  *
  */
-//@AcraCore(buildConfigClass = BuildConfig.class)
-//@AcraMailSender(mailTo = "simplicity@creativetrendsapps.com")
 public class SimplicityApplication extends Application {
 
     @SuppressLint("StaticFieldLeak")
@@ -35,24 +32,18 @@ public class SimplicityApplication extends Application {
         PreferenceManager.setDefaultValues(mContext, R.xml.flags_settings, true);
         PreferenceManager.setDefaultValues(mContext, R.xml.plugin_settings, true);
         createNotificationChannel();
-        //AppCenter.start(this, "5323570c-369d-40cc-9303-7a9abd2f06ba", Analytics.class, Crashes.class);
-        /*new Instabug.Builder(this, "7cc1d3738c052930125dd874da011eb8")
-                .setInvocationEvents(InstabugInvocationEvent.FLOATING_BUTTON, InstabugInvocationEvent.SCREENSHOT)
-                .build();*/
+       
 
     }
 
     private void createNotificationChannel() {
         try {
-            // Create the NotificationChannel, but only on API 26+ because
-            // the NotificationChannel class is new and not in the support library
+           
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                //widget channel
                 CharSequence widget_name = getString(R.string.notifications_widget);
                 String widget_description = getString(R.string.notifications_widget_description);
                 int wimp = NotificationManager.IMPORTANCE_LOW;
 
-                //widget
                 NotificationChannel channel5 = new NotificationChannel(getString(R.string.notification_widget_channel), widget_name, wimp);
                 channel5.setDescription(widget_description);
                 channel5.setShowBadge(true);
