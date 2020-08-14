186c88a14f22d6905b71a6acfd21f2064e79948e
Creative Trends <creativetrendzappsllc@gmail.com>
2020-08-13 22:41:08 -0400

    Update SimplicityAccount.java

--- app/src/main/java/com/creativetrends/app/simplicity/activities/SimplicityAccount.java
+++ app/src/main/java/com/creativetrends/app/simplicity/activities/SimplicityAccount.java
@@ -1,5 +1,6 @@
 package com.creativetrends.app.simplicity.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -7,17 +8,21 @@ import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.AppCompatTextView;
 import androidx.appcompat.widget.Toolbar;
 
 import com.creativetrends.app.simplicity.ui.Cardbar;
 import com.creativetrends.app.simplicity.utils.ExportUtils;
+import com.creativetrends.app.simplicity.utils.StaticUtils;
 import com.creativetrends.app.simplicity.utils.UserPreferences;
 import com.creativetrends.simplicity.app.R;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -27,15 +32,17 @@ import java.util.Objects;
 
 public class SimplicityAccount extends BaseActivity {
     TextInputEditText editText1, editText2;
-    AppCompatButton verify;
+    MaterialButton verify;
     Toolbar toolbar;
     AppCompatTextView forgot, create;
     int attempts = 2;
+    AlertDialog PostDialog;
+    ProgressBar progressBar;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        if(UserPreferences.getBoolean("dark_mode", false)){
+        if (UserPreferences.getBoolean("dark_mode", false)) {
             setTheme(R.style.SimplicityAccountThemeDark);
         }
         super.onCreate(savedInstanceState);
@@ -72,75 +79,66 @@ public class SimplicityAccount extends BaseActivity {
                 }
                 return false;
             });
-        }catch (NullPointerException i){
+        } catch (NullPointerException i) {
             i.printStackTrace();
-        }catch (Exception ignored){
+        } catch (Exception ignored) {
 
         }
 
 
     }
-
-
 
 
     private void signIn(String email, String password) {
         if (!validateForm()) {
             return;
         }
-        try{
+        try {
+            deleteCache();
             mAuth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, task -> {
                         if (task.isSuccessful()) {
-                            if(currentUser != null) {
-                                Cardbar.snackBar(this, "Welcome back" + " " + mAuth.getCurrentUser().getDisplayName() + "!", false).show();
-                            }else{
+                            if (Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName() != null) {
+                                Cardbar.snackBar(this, "Welcome back," + " " + Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName() + "!", false).show();
+                            } else {
                                 Cardbar.snackBar(this, "Welcome back!", false).show();
-                                Log.i("User", mAuth.getCurrentUser().getUid());
-                                try{
-                                    File bh = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + File.separator + getResources().getString(R.string.app_name) + File.separator + "Simplicity Backups" + File.separator);
-                                    if (!bh.exists()) {
-                                        //noinspection ResultOfMethodCallIgnored
-                                        bh.mkdirs();
-                                    }
-                                    String extStorageDirectory = bh.toString();
-                                    File file = new File(extStorageDirectory, mAuth.getCurrentUser().getUid() + ".sbh");
-                                    ExportUtils.writeToFile(file, this);
-                                }catch (Exception i){
-                                    i.printStackTrace();
-
-                                }
-                                new Handler().postDelayed(() -> {
-                                    try {
-                                        downloadFromFirebase();
-                                    } catch (Exception e) {
-                                        e.printStackTrace();
-                                    }
-                                }, 4000);
                             }
-                            updateUI();
+                            //Log.i("User", Objects.requireNonNull(mAuth.getCurrentUser()).getUid());
+                            UserPreferences.putString("user", Objects.requireNonNull(mAuth.getCurrentUser()).getUid());
+
+                            new Handler().postDelayed(() -> {
+                                try {
+                                    downloadFromFirebase();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }, 5000);
                         } else {
-                            Cardbar.snackBar(this,"Authentication failed.", true).show();
+                            if (PostDialog != null && PostDialog.isShowing()) {
+                                PostDialog.dismiss();
+                            }
+                            UserPreferences.putString("user", StaticUtils.getRandomString(28));
+                            Cardbar.snackBar(this, "Authentication failed.", true).show();
                             Objects.requireNonNull(editText2.getText()).clear();
                             attempts--;
-                            if(attempts == 0){
+                            if (attempts == 0) {
                                 forgot.setVisibility(View.VISIBLE);
-                            }else{
+                            } else {
                                 forgot.setVisibility(View.INVISIBLE);
                             }
                         }
                     });
-        }catch (NullPointerException i){
+        } catch (NullPointerException i) {
             i.printStackTrace();
-        }catch (Exception ignored){
+        } catch (Exception ignored) {
 
         }
     }
 
 
-    private boolean validateForm () {
+    private boolean validateForm() {
         boolean valid = true;
-        try{
+        try {
             String email = Objects.requireNonNull(editText1.getText()).toString();
             if (TextUtils.isEmpty(email)) {
                 editText1.setError("Required.");
@@ -156,29 +154,29 @@ public class SimplicityAccount extends BaseActivity {
             } else {
                 editText2.setError(null);
             }
-        }catch (NullPointerException i){
+        } catch (NullPointerException i) {
             i.printStackTrace();
-        }catch (Exception ignored){
+        } catch (Exception ignored) {
 
         }
 
         return valid;
     }
 
-    private void resetPass(String email){
-        try{
+    private void resetPass(String email) {
+        try {
             mAuth.sendPasswordResetEmail(email)
                     .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
-                            Cardbar.snackBar(this,"Password reset email sent to " + Objects.requireNonNull(editText1.getText()).toString(), true).show();
+                            Cardbar.snackBar(this, "Password reset email sent to " + Objects.requireNonNull(editText1.getText()).toString(), true).show();
 
                         }
                     });
             attempts = 2;
 
-        }catch (NullPointerException i){
+        } catch (NullPointerException i) {
             i.printStackTrace();
-        }catch (Exception ignored){
+        } catch (Exception ignored) {
 
         }
     }
@@ -192,16 +190,16 @@ public class SimplicityAccount extends BaseActivity {
                 finish();
             } else if (!Objects.requireNonNull(editText1.getText()).toString().isEmpty() || !Objects.requireNonNull(editText2.getText()).toString().isEmpty()) {
                 editText1.getText().clear();
-                editText2.getText().clear();
+                Objects.requireNonNull(editText2.getText()).clear();
             }
             if (attempts == 0) {
                 forgot.setVisibility(View.VISIBLE);
             } else {
                 forgot.setVisibility(View.INVISIBLE);
             }
-        }catch (NullPointerException i){
+        } catch (NullPointerException i) {
             i.printStackTrace();
-        }catch (Exception ignored){
+        } catch (Exception ignored) {
 
         }
     }
@@ -216,40 +214,69 @@ public class SimplicityAccount extends BaseActivity {
         super.onDestroy();
     }
 
-    private void loadMain(){
-        try{
-            if(currentUser != null) {
-                File bh = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + File.separator + getResources().getString(R.string.app_name) + File.separator + "Simplicity Backups" + File.separator, currentUser.getUid() + ".sbh");
-                ExportUtils.writeToFile(bh, this);
+    private void loadMain() {
+        try {
+            final File root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            String pathToMyAttachedFile = File.separator + getResources().getString(R.string.app_name) + File.separator + "Simplicity Backups" + File.separator + UserPreferences.getString("user", "") + ".sbh";
+            File file = new File(root, pathToMyAttachedFile);
+            ExportUtils.readFromFile(file, this);
+            if (PostDialog != null && PostDialog.isShowing()) {
+                PostDialog.dismiss();
             }
-        }catch (Exception i){
+        } catch (Exception i) {
             i.printStackTrace();
-
         }
         finish();
     }
 
 
-    private void downloadFromFirebase(){
-        try{
-            StorageReference proimage = FirebaseStorage.getInstance().getReference(Objects.requireNonNull(mAuth.getCurrentUser()).getUid() + "/simplicity_backup/" + mAuth.getCurrentUser().getUid() + ".sbh");
-            File bh = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + File.separator + getResources().getString(R.string.app_name) + File.separator + "Simplicity Backups" + File.separator, mAuth.getCurrentUser().getUid() + ".sbh");
-            ExportUtils.readFromFile(bh, this);
-            proimage.getFile(bh).addOnSuccessListener(taskSnapshot -> {
+    private void downloadFromFirebase() {
+        try {
+            //Log.e("User?",UserPreferences.getString("user", ""));
+            String fileName = UserPreferences.getString("user", "");
+            StorageReference proimage = FirebaseStorage.getInstance().getReference(UserPreferences.getString("user", "") + "/simplicity_backup/" + UserPreferences.getString("user", "") + ".sbh");
+            final File rootPath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), File.separator + getResources().getString(R.string.app_name) + File.separator + "Simplicity Backups");
+            if (!rootPath.exists()) {
+                //noinspection ResultOfMethodCallIgnored
+                rootPath.mkdirs();
+                Log.i("created dir", rootPath.getPath());
+            } else {
+                Log.i("", "");
+            }
+            final File localFile = new File(rootPath, fileName + ".sbh");
+
+            proimage.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
                 Log.d("Downloaded", "got backup from Firebase");
+                loadMain();
             }).addOnFailureListener(exception ->
                     Log.d("Failed from Firebase", Objects.requireNonNull(exception).toString()));
-        }catch (NullPointerException i){
+            loadMain();
+        } catch (NullPointerException i) {
             i.printStackTrace();
-        }catch (Exception ignored){
+        } catch (Exception ignored) {
 
         }
     }
 
 
-    private void updateUI() {
-        loadMain();
-
+    private void deleteCache() {
+        try {
+            LayoutInflater inflater = getLayoutInflater();
+            @SuppressLint("InflateParams")
+            View alertLayout = inflater.inflate(R.layout.account_dialog, null);
+            progressBar = alertLayout.findViewById(R.id.prog);
+            progressBar.setIndeterminate(true);
+            AlertDialog.Builder progress = new AlertDialog.Builder(SimplicityAccount.this);
+            progress.setTitle("Account Sync");
+            progress.setMessage("Syncing bookmarks and history");
+            progress.setCancelable(false);
+            progress.setView(alertLayout);
+            PostDialog = progress.create();
+            PostDialog.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 
 }
