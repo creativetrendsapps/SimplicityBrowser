5c60a8ae0fe4e2f17c82e4168c6fa2160f336964
Creative Trends <creativetrendzappsllc@gmail.com>
2020-08-13 22:55:40 -0400

    Update SimplicityAccount.java

--- app/src/main/java/com/creativetrends/app/simplicity/activities/SimplicityAccount.java
+++ app/src/main/java/com/creativetrends/app/simplicity/activities/SimplicityAccount.java
@@ -1,6 +1,5 @@
 package com.creativetrends.app.simplicity.activities;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -63,144 +62,112 @@ public class SimplicityAccount extends BaseActivity {
 
         AppCompatTextView copyright = findViewById(R.id.copy);
         copyright.setText("");
-        try {
-            create.setOnClickListener(v -> {
-                Intent intent = new Intent(SimplicityAccount.this, NewSimplicityAccount.class);
-                startActivity(intent);
-                finish();
+        create.setOnClickListener(v -> {
+            Intent intent = new Intent(SimplicityAccount.this, NewSimplicityAccount.class);
+            startActivity(intent);
+            finish();
 
-            });
-            verify.setOnClickListener(v -> signIn(Objects.requireNonNull(editText1.getText()).toString(), Objects.requireNonNull(editText2.getText()).toString()));
-            forgot.setOnClickListener(v -> resetPass(Objects.requireNonNull(editText1.getText()).toString()));
-            editText2.setOnKeyListener((v, keyCode, event) -> {
-                if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                    signIn(Objects.requireNonNull(editText1.getText()).toString(), Objects.requireNonNull(editText2.getText()).toString());
-                    return true;
-                }
-                return false;
-            });
-        } catch (NullPointerException i) {
-            i.printStackTrace();
-        } catch (Exception ignored) {
-
-        }
+        });
+        verify.setOnClickListener(v -> signIn(Objects.requireNonNull(editText1.getText()).toString(), Objects.requireNonNull(editText2.getText()).toString()));
+        forgot.setOnClickListener(v -> resetPass(Objects.requireNonNull(editText1.getText()).toString()));
+        editText2.setOnKeyListener((v, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                signIn(Objects.requireNonNull(editText1.getText()).toString(), Objects.requireNonNull(editText2.getText()).toString());
+                return true;
+            }
+            return false;
+        });
 
 
     }
 
 
+    //verify account and sign in
     private void signIn(String email, String password) {
         if (!validateForm()) {
             return;
         }
-        try {
-            deleteCache();
-            mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, task -> {
-                        if (task.isSuccessful()) {
-                            if (Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName() != null) {
-                                Cardbar.snackBar(this, "Welcome back," + " " + Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName() + "!", false).show();
-                            } else {
-                                Cardbar.snackBar(this, "Welcome back!", false).show();
-                            }
-                            //Log.i("User", Objects.requireNonNull(mAuth.getCurrentUser()).getUid());
-                            UserPreferences.putString("user", Objects.requireNonNull(mAuth.getCurrentUser()).getUid());
-
-                            new Handler().postDelayed(() -> {
-                                try {
-                                    downloadFromFirebase();
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            }, 5000);
+        accountDialog();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        if (Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName() != null) {
+                            Cardbar.snackBar(this, "Welcome back," + " " + Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName() + "!", false).show();
                         } else {
-                            if (PostDialog != null && PostDialog.isShowing()) {
-                                PostDialog.dismiss();
-                            }
-                            UserPreferences.putString("user", StaticUtils.getRandomString(28));
-                            Cardbar.snackBar(this, "Authentication failed.", true).show();
-                            Objects.requireNonNull(editText2.getText()).clear();
-                            attempts--;
-                            if (attempts == 0) {
-                                forgot.setVisibility(View.VISIBLE);
-                            } else {
-                                forgot.setVisibility(View.INVISIBLE);
-                            }
+                            Cardbar.snackBar(this, "Welcome back!", false).show();
                         }
-                    });
-        } catch (NullPointerException i) {
-            i.printStackTrace();
-        } catch (Exception ignored) {
-
-        }
+                        new Handler().postDelayed(() -> {
+                            try {
+                                downloadFromFirebase();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }, 5000);
+                    } else {
+                        if (isDestroyed() && PostDialog != null && PostDialog.isShowing()) {
+                            PostDialog.dismiss();
+                        }
+                        UserPreferences.putString("user", StaticUtils.getRandomString(28));
+                        Cardbar.snackBar(this, "Authentication failed.", true).show();
+                        Objects.requireNonNull(editText2.getText()).clear();
+                        attempts--;
+                        if (attempts == 0) {
+                            forgot.setVisibility(View.VISIBLE);
+                        } else {
+                            forgot.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
     }
 
 
+    //make user all required fields are complete when updating a profile
     private boolean validateForm() {
         boolean valid = true;
-        try {
-            String email = Objects.requireNonNull(editText1.getText()).toString();
-            if (TextUtils.isEmpty(email)) {
-                editText1.setError("Required.");
-                valid = false;
-            } else {
-                editText1.setError(null);
-            }
-
-            String password = Objects.requireNonNull(editText2.getText()).toString();
-            if (TextUtils.isEmpty(password)) {
-                editText2.setError("Required.");
-                valid = false;
-            } else {
-                editText2.setError(null);
-            }
-        } catch (NullPointerException i) {
-            i.printStackTrace();
-        } catch (Exception ignored) {
-
+        String email = Objects.requireNonNull(editText1.getText()).toString();
+        if (TextUtils.isEmpty(email)) {
+            editText1.setError("Required.");
+            valid = false;
+        } else {
+            editText1.setError(null);
         }
 
+        String password = Objects.requireNonNull(editText2.getText()).toString();
+        if (TextUtils.isEmpty(password)) {
+            editText2.setError("Required.");
+            valid = false;
+        } else {
+            editText2.setError(null);
+        }
         return valid;
     }
 
+    //offer reset email after a user fails to login
     private void resetPass(String email) {
-        try {
-            mAuth.sendPasswordResetEmail(email)
-                    .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            Cardbar.snackBar(this, "Password reset email sent to " + Objects.requireNonNull(editText1.getText()).toString(), true).show();
+        mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Cardbar.snackBar(this, "Password reset email sent to " + Objects.requireNonNull(editText1.getText()).toString(), true).show();
 
-                        }
-                    });
-            attempts = 2;
-
-        } catch (NullPointerException i) {
-            i.printStackTrace();
-        } catch (Exception ignored) {
-
-        }
+                    }
+                });
+        attempts = 2;
     }
 
 
     @Override
     protected void onResume() {
         super.onResume();
-        try {
-            if (currentUser != null) {
-                finish();
-            } else if (!Objects.requireNonNull(editText1.getText()).toString().isEmpty() || !Objects.requireNonNull(editText2.getText()).toString().isEmpty()) {
-                editText1.getText().clear();
-                Objects.requireNonNull(editText2.getText()).clear();
-            }
-            if (attempts == 0) {
-                forgot.setVisibility(View.VISIBLE);
-            } else {
-                forgot.setVisibility(View.INVISIBLE);
-            }
-        } catch (NullPointerException i) {
-            i.printStackTrace();
-        } catch (Exception ignored) {
-
+        if (currentUser != null) {
+            finish();
+        } else if (!Objects.requireNonNull(editText1.getText()).toString().isEmpty() || !Objects.requireNonNull(editText2.getText()).toString().isEmpty()) {
+            editText1.getText().clear();
+            Objects.requireNonNull(editText2.getText()).clear();
+        }
+        if (attempts == 0) {
+            forgot.setVisibility(View.VISIBLE);
+        } else {
+            forgot.setVisibility(View.INVISIBLE);
         }
     }
 
@@ -214,69 +181,49 @@ public class SimplicityAccount extends BaseActivity {
         super.onDestroy();
     }
 
+    //read the file we just downloaded to restore bookmarks and history
     private void loadMain() {
-        try {
-            final File root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-            String pathToMyAttachedFile = File.separator + getResources().getString(R.string.app_name) + File.separator + "Simplicity Backups" + File.separator + UserPreferences.getString("user", "") + ".sbh";
-            File file = new File(root, pathToMyAttachedFile);
-            ExportUtils.readFromFile(file, this);
-            if (PostDialog != null && PostDialog.isShowing()) {
-                PostDialog.dismiss();
-            }
-        } catch (Exception i) {
-            i.printStackTrace();
+        final File root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        String pathToMyAttachedFile = File.separator + getResources().getString(R.string.app_name) + File.separator + "Simplicity Backups" + File.separator + UserPreferences.getString("user", "") + ".sbh";
+        File file = new File(root, pathToMyAttachedFile);
+        ExportUtils.readFromFile(file, this);
+        if (isDestroyed() && PostDialog != null && PostDialog.isShowing()) {
+            PostDialog.dismiss();
         }
         finish();
     }
 
-
+    //download and save bookmarks and history
     private void downloadFromFirebase() {
-        try {
-            //Log.e("User?",UserPreferences.getString("user", ""));
-            String fileName = UserPreferences.getString("user", "");
-            StorageReference proimage = FirebaseStorage.getInstance().getReference(UserPreferences.getString("user", "") + "/simplicity_backup/" + UserPreferences.getString("user", "") + ".sbh");
-            final File rootPath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), File.separator + getResources().getString(R.string.app_name) + File.separator + "Simplicity Backups");
-            if (!rootPath.exists()) {
-                //noinspection ResultOfMethodCallIgnored
-                rootPath.mkdirs();
-                Log.i("created dir", rootPath.getPath());
-            } else {
-                Log.i("", "");
-            }
-            final File localFile = new File(rootPath, fileName + ".sbh");
+        String fileName = currentUser.getUid();
+        StorageReference proimage = FirebaseStorage.getInstance().getReference(currentUser.getUid() + "/your_path/" + UserPreferences.getString("user", "") + ".sbh");
+        final File rootPath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), File.separator + getResources().getString(R.string.app_name) + File.separator + "Simplicity Backups");
+        if (!rootPath.exists()) {
+            //noinspection ResultOfMethodCallIgnored
+            rootPath.mkdirs();
+        }
+        final File localFile = new File(rootPath, fileName + ".sbh");
 
-            proimage.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
-                Log.d("Downloaded", "got backup from Firebase");
-                loadMain();
-            }).addOnFailureListener(exception ->
-                    Log.d("Failed from Firebase", Objects.requireNonNull(exception).toString()));
+        proimage.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
+            Log.d("Downloaded", "got backup from Firebase");
             loadMain();
-        } catch (NullPointerException i) {
-            i.printStackTrace();
-        } catch (Exception ignored) {
-
-        }
+        }).addOnFailureListener(exception ->
+                Log.d("Failed from Firebase", Objects.requireNonNull(exception).toString()));
+        loadMain();
     }
 
-
-    private void deleteCache() {
-        try {
-            LayoutInflater inflater = getLayoutInflater();
-            @SuppressLint("InflateParams")
-            View alertLayout = inflater.inflate(R.layout.account_dialog, null);
-            progressBar = alertLayout.findViewById(R.id.prog);
-            progressBar.setIndeterminate(true);
-            AlertDialog.Builder progress = new AlertDialog.Builder(SimplicityAccount.this);
-            progress.setTitle("Account Sync");
-            progress.setMessage("Syncing bookmarks and history");
-            progress.setCancelable(false);
-            progress.setView(alertLayout);
-            PostDialog = progress.create();
-            PostDialog.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    //accounts dialog to show as we download user bookmarks and history
+    private void accountDialog() {
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.account_dialog, null);
+        progressBar = alertLayout.findViewById(R.id.prog);
+        progressBar.setIndeterminate(true);
+        AlertDialog.Builder progress = new AlertDialog.Builder(SimplicityAccount.this);
+        progress.setTitle("Account Sync");
+        progress.setMessage("Syncing bookmarks and history");
+        progress.setCancelable(false);
+        progress.setView(alertLayout);
+        PostDialog = progress.create();
+        PostDialog.show();
     }
-
-
 }
